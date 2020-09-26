f9423ca787ff5ca897011249482b8e4f830a7e0c
Justine Manaloto <justine.kim.manaloto@gmail.com>
2020-09-26 23:42:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def reverse_each_word(string)
   array = string.split
-  new_array = array.collect {|value| value.reverse}
+  new_array = array.each {|value| value.reverse}
   new_array.join
   new_array
   #binding.pry
