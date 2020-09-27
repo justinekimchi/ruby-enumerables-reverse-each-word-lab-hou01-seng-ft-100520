07124c3d150cf7a6b85ce9cc9a54d22048372f3b
Justine Manaloto <justine.kim.manaloto@gmail.com>
2020-09-27 00:03:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def reverse_each_word(string)
-  array = string.split("into")
+  array = string.split
   new_array = array.collect {|value| value.reverse}
   new_array.join
     #binding.pry
